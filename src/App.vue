48f0a03b8078769b67f078c88dc773238dcd4db3
Brian Bickett <brian@bickett.dev>
2023-03-06 10:47:17 -0500

    add ribbon

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div class="container-xl">
 
     <div class="page">
+      <div class="ribbon"><a href="/react"><span><small>See in<br/></small><fa-icon icon="fa-brands fa-react"></fa-icon> React</span></a></div>
       <div class="container-fluid">
         <Suspense>
           <router-view></router-view>
@@ -10,10 +11,11 @@
       </div>
     </div>
 
+    <div class="built-with">
+      <a href="https://github.com/bbickett/bbresume-vue">
+        Built with <fa-icon class="px-1" icon="fa-brands fa-vuejs" size="lg"></fa-icon> Vue
+      </a>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-
-
-</script>
